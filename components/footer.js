91669bebb74875eb0b779fd71256724b8a6f09bc
Alberto Beiztegui Casado <albertobeiz@gmail.com>
2022-10-24 14:56:46 +0200

    Remove tops

--- components/footer.js
+++ components/footer.js
@@ -1,27 +1,9 @@
 import Container from './container';
 
 export default function Footer() {
-  const listNames = ['Videojuegos', 'TV', 'Películas'];
   return (
     <footer className="bg-accent-1 border-t border-accent-2">
-      <Container>
-        <div className="py-8 flex flex-col lg:flex-row items-center">
-          <div className="flex flex-col text-left">
-            <h3 className="text-xl font-semibold text-left mb-1">Top 2022</h3>
-            {listNames.map((listName) => (
-              <a
-                key={listName}
-                href={`/top/${listName.toLowerCase()}`}
-                className="text-sm text-gray-600 hover:text-gray-900"
-              >
-                {listName}
-              </a>
-            ))}
-          </div>
-
-          <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2"></div>
-        </div>
-      </Container>
+      <Container></Container>
     </footer>
   );
 }
